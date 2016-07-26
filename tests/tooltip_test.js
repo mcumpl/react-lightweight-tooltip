ff3f87eace338cb2e22fca14ed6d3f4fa789e07f
Michal Cumpl <michal.cumpl@gmail.com>
2016-07-26 16:34:06 +0200

    Added 2 tests

--- tests/tooltip_test.js
+++ tests/tooltip_test.js
@@ -1,15 +1,10 @@
 import React from 'react';
-//import ReactDOM from 'react-dom';
-//import TestUtils from 'react-addons-test-utils';
+import TestUtils from 'react-addons-test-utils';
 import {Tooltip} from '../src/index';
 
 export default class DefaultTooltip extends React.Component {
   render() {
-    return (
-      <div>
-        <Tooltip content="Yes, the default one" ref="tooltip">Simple black tooltip</Tooltip>
-      </div>
-    );
+    return <Tooltip content="Yes, the default one" ref="tooltip">Simple black tooltip</Tooltip>;
   }
 }
 
@@ -18,43 +13,10 @@ describe('Tooltip', () => {
     expect(Tooltip).to.exist;
     expect(Tooltip).to.be.instanceof(Function);
   });
-/*
-  it('should change state on the location', () => {
 
-
-
-var node = this.refs.button;
-ReactTestUtils.Simulate.click(node);
-
-
-    const mockGeolocationProvider = {
-      getCurrentPosition(onSuccess) {
-        return onSuccess({
-          coords: {
-            latitude: 50,
-            longitude: 20,
-          },
-        })
-      },
-    };
-
-    const Wrapped = geolocated({
-      geolocationProvider: mockGeolocationProvider,
-    })(SimpleComponent);
-
-    const rendered = TestUtils.renderIntoDocument(<Wrapped />);
-    const renderedNode = ReactDOM.findDOMNode(rendered);
-
-    expect(renderedNode.textContent).to.equal('50, 20');
+  it('default state should be equal {visible:false}', () => {
+    const rendered = TestUtils.renderIntoDocument(<DefaultTooltip />);
+    const node = rendered.refs.tooltip;
+    expect(node.state.visible).to.equal(false);
   });
-
-  it('should throw on invalid geolocation provider', () => {
-    const Wrapped = geolocated({
-      geolocationProvider: {},
-    })(SimpleComponent);
-
-    expect(() => TestUtils.renderIntoDocument(<Wrapped />)).to.throw();
-  });
-
-  */
 });
